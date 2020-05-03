a5af5f05a3e923c0f6c96ed488d37e1e1a25db26
Jared Crouse <jaredcrouse85@gmail.com>
2020-05-03 00:11:05 -0600

    Verbose argument added, infinite loop actually fixed

--- parser.py
+++ parser.py
@@ -21,20 +21,20 @@ def begin_parse(file_arg, create_dirs = False):
 	return location, everything
 
 
-def check_lang(the_list):
+def check_lang(the_list, verbose = False):
 	for line in the_list:
 		cpp_ind = line.find('lang-cpp')
 		java_ind = line.find('lang-java')
 		python_ind = line.find('lang-python')
 		if cpp_ind != -1:
-			replace_and_save(line, '.cpp')
+			replace_and_save(line, '.cpp', verbose)
 		if java_ind != -1:
-			replace_and_save(line, '.java')
+			replace_and_save(line, '.java', verbose)
 		if python_ind != -1:
-			replace_and_save(line, '.py')
+			replace_and_save(line, '.py', verbose)
 
 
-def replace_and_save(line, ext):
+def replace_and_save(line, ext, verbose):
 	eight_zeros = '00000000'
 	if (line[0] == '"'):
 		line = line[1:]
@@ -43,29 +43,37 @@ def replace_and_save(line, ext):
 	line = line.replace('&#xD;&#xA;', '\n')
 
 	while True:
-		start_tag_ind = line.find('<!--')
+		start_tag_ind = line.find('<!-- ')
 		if start_tag_ind == -1:
 			break
-		end_tag_ind = line.find('-->')
+		end_tag_ind = line.find(' -->')
 		if end_tag_ind == -1:
 			break
-		remove_tag = line[start_tag_ind:end_tag_ind + 3]
+		if start_tag_ind > end_tag_ind:
+			break
+		remove_tag = line[start_tag_ind:end_tag_ind + 4]
 		line = line.replace(remove_tag, '')
 
 	if ext == '.cpp':
 		file_name = eight_zeros[0:8-len(str(cpp_count))]
 		with open(path + '/cpp/' + file_name + str(cpp_count) + ext, 'w') as new_cpp_snippet:
 			new_cpp_snippet.write(line)
+		if verbose:
+			print(file_name + str(cpp_count) + ext)
 		increment_cpp()
 	elif ext == '.java':
 		file_name = eight_zeros[0:8-len(str(java_count))]
 		with open(path + '/java/' + file_name + str(java_count) + ext, 'w') as new_java_snippet:
 			new_java_snippet.write(line)
+		if verbose:
+			print(file_name + str(java_count) + ext)
 		increment_java()
 	elif ext == '.py':
 		file_name = eight_zeros[0:8-len(str(python_count))]
 		with open(path + '/python/' + file_name + str(python_count) + ext, 'w') as new_python_snippet:
 			new_python_snippet.write(line)
+		if verbose:
+			print(file_name + str(python_count) + ext)
 		increment_python()
 
 
@@ -116,6 +124,7 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--create_dir', required = False, action = 'store_true')
 	parser.add_argument('-c', '--file_count', required = False, action = 'store_true')
 	parser.add_argument('-f', '--parse_file', required = True)
+	parser.add_argument('-v', '--verbose', required = False, action = 'store_true')
 	args = parser.parse_args()
 	file = args.parse_file
 
@@ -125,7 +134,10 @@ if __name__ == '__main__':
 		path, all_lines = begin_parse(file)
 
 	get_file_count()
-	check_lang(all_lines)
+	if args.verbose == True:
+		check_lang(all_lines, True)
+	else:
+		check_lang(all_lines)
 
 	if args.file_count == True:
 		c, j, p = get_file_count(False)
